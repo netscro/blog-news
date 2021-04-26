2588f466d2f81b1c5d206218b6a679790c8afed1
netscro <kovalev_evgeniy@list.ru>
2021-04-26 14:36:23 +0300

    fix flake8

--- news/serializers.py
+++ news/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, pagination
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from news.models import Category, Post, Comment
