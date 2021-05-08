be3d7921a552e4a3cc64a9022551e3491c2d843b
netscro <kovalev_evgeniy@list.ru>
2021-05-08 21:00:50 +0300

    some fix

--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_value("SECRET_KEY")
-# SECRET_KEY = 'c_o+4@civ!c-!4!*b!=2ibk$&jplg@tc76t-twm29c!t1_2(y('
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
